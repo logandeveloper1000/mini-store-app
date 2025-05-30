73bcbde6c63fb324eda841ad0b3e5e1accae3435
connectionaccounting-systems <gabriel.connectionacc@gmail.com>
2025-05-29 22:07:10 -0400

    updating

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD54kGOh-xLMeAh_RX5bu8TRMZliNhOB4A",
-  authDomain: "portfolio-logandeveloper.firebaseapp.com",
-  projectId: "portfolio-logandeveloper",
-  storageBucket: "portfolio-logandeveloper.firebasestorage.app",
-  messagingSenderId: "546326089695",
-  appId: "1:546326089695:web:45e2f9855cc9a3ef96fe6a",
-  measurementId: "G-XC9430XVZ1"
+  apiKey: "AIzaSyB1TWj5WC5yEWYmQCkc8pqx5xvrkhEbN3A",
+  authDomain: "logan-portolio.firebaseapp.com",
+  projectId: "logan-portolio",
+  storageBucket: "logan-portolio.firebasestorage.app",
+  messagingSenderId: "254880871321",
+  appId: "1:254880871321:web:55b28084bebdda374b5908",
+  measurementId: "G-9V8FZZPC4Y"
 };
 
 // Initialize Firebase
